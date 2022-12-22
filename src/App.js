25abd8f71dc16538ab4073a80f8f8486a5f5d8c7
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-12-22 13:47:04 +0530

    company name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import companyData from './Company Details.json';
+import UserData from './User Details.json';
+import addressData from './Address.json';
+import { useState } from 'react';
 
+console.log({ companyData }, { UserData }, { addressData });
 function App() {
+  const [initialValue, setInitialValue] = useState();
+  const [nameSearchValue, setNameSearchValue] = useState([]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    setInitialValue(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const nameAndID = companyData.filter(
+      (item) => item.company_name == initialValue || item.id == initialValue
+    );
+    setNameSearchValue(nameAndID);
+    console.log({ nameAndID }, { companyData }, { initialValue });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        Enter value: {initialValue} <br />
+        <input
+          type="text"
+          name="name"
+          value={initialValue}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        {nameSearchValue &&
+          nameSearchValue.length > 0 &&
+          nameSearchValue.map((item) => {
+            <ol>
+              <li>
+                name: {item.company_name} + id: {item.id} + total employee:{item.no_of_employees}
+              </li>
+            </ol>;
+          })}
+      </div>
     </div>
   );
 }
